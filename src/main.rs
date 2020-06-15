99802aef14fc7d24c1af3131d52c8f60762fe7ca
Tilo Reinhardt <tilo.reinhardt@gmail.com>
2020-06-15 18:05:15 +0200

    First basic run with Travis CI integration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::error::Error;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    match run("input.txt") {
+        Ok(t) => println!("Memory Cell 0: {}", t),
+        Err(e) => {
+            println!("Application error: {}", e);
+            process::exit(1)
+        }
+    }
+}
+
+fn run(filename: &str) -> Result<usize, Box<dyn Error>> {
+
+    let contents = fs::read_to_string(filename)?;
+    let mut m: usize;
+    let mut mem = Vec::new();
+
+    let mut i: usize = 0;
+    for code in contents.split(',') {
+        m = code.trim().parse().unwrap();
+        println!("{} -> {}", i, m);
+        mem.push(m);
+        i += 1;
+    }
+
+    exec(&mem);
+
+    Ok(mem[0])
+}
+
+fn exec(mem: &Vec<usize>) {
+
 }
